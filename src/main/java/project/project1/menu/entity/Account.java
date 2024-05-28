fda27e3f1e36fbce01fd4b6b1d6331e3bb3206a2
Luu Dinh The Tai <luudinhthetai741@gmail.com>
2024-05-28 22:01:05 +0700

    first commit

---
+++ src/main/java/project/project1/menu/entity/Account.java
@@ -0,0 +1,66 @@
+package project.project1.menu.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Account {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "username")
+    private int username;
+    @Basic
+    @Column(name = "password")
+    private String password;
+    @Basic
+    @Column(name = "roles")
+    private String roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsername() {
+        return username;
+    }
+
+    public void setUsername(int username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return id == account.id && username == account.username && Objects.equals(password, account.password) && Objects.equals(roles, account.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, roles);
+    }
+}
---
+++ src/test/java/project/project1/menu/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.project1.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
